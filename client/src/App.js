97ff726d4130947f79e9f53e6fdf5ff427c21201
aaronlwan <aaronwan@stanford.edu>
2023-03-04 13:44:27 -0800

    testing frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { React, useState} from 'react';
+import { GetMapHtml } from './GetMapHtml';
+import { GetLatLon } from './GetLatLon';
+import states from './statecodes.json'
 
 function App() {
+  const [mapLoaded, setMapLoaded] = useState(false);
+  const [mapHtml, setMapHtml] = useState("Loading map...");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const stateCodes = Object.keys(states);
+
+  const onSubmit = () => {
+    const latLonData = GetLatLon(city, state, "US");
+    latLonData.then((res)=>{
+      const mapData = GetMapHtml(res.lat, res.lon, 1000);
+      mapData.then((res) => {
+        setMapHtml(res.html);
+      })
+      setMapLoaded(true);
+    })
+  }
+
+  const Map = (props) => {
+    const loaded = props.loaded;
+    if (loaded) {
+      return (
+        <div dangerouslySetInnerHTML={{__html: mapHtml}}/>
+      )
+    } else {
+      return (
+        "Map Placeholder"
+      )
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="input-wrapper">
+        <label>City:</label>
+        <input className="city-input" value={city} onChange={({target})=> {setCity(target.value)}}/>
+        <label>State:</label>
+        <select className="state-input" value={state} onChange={({target}) =>{setState(target.value)}}>
+          <option></option>
+          {stateCodes.map((code) => <option key={code}>{code}</option>)}
+        </select>
+        <div className="submit-button" onClick={onSubmit}>GoSolar!</div>
+      </div>
+      <div className="map-wrapper">
+        <Map loaded={mapLoaded}/>
+      </div>
     </div>
   );
 }
