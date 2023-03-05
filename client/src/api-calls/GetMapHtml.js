f969abcf334f12d9de7e3774d41c50d2b83ccad2
aaronlwan <aaronwan@stanford.edu>
2023-03-05 03:23:04 -0800

    set up for data display

--- client/src/api-calls/GetMapHtml.js
+++ client/src/api-calls/GetMapHtml.js
@@ -1,11 +1,13 @@
-export async function GetMapHtml(lat, lon, r)  {
+export async function GetMapHtml(lat, lon, r, city, state)  {
     const params = {
         'method': 'GET',
     }
     let response = await fetch('http://127.0.0.1:5000/latlontomap?' + new URLSearchParams({
         "lat": lat,
         "lon": lon,
-        "r": r
+        "r": r,
+        "city": city,
+        "state": state
     }), params);
     const result = response.json();
     return result;
