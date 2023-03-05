91117fd3898ac95dc678ee6bfe7e06c37a90ea31
aaronlwan <aaronwan@stanford.edu>
2023-03-04 16:52:20 -0800

    home page setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import CallToActionWithAnnotation from './components/home-page/Home';
 import MapPage from './components/map-page/MapPage';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route exact path="/home" element={<CallToActionWithAnnotation/>}/>
           <Route exact path="/site-sourcing-map" element={<MapPage/>}/>
         </Routes>
       </BrowserRouter>
